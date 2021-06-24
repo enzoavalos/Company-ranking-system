5047163a68ba72e6ebd52c6c52933cf81bd8413d
Avalos Enzo <82390064+enzoavalos@users.noreply.github.com>
2021-06-24 18:06:48 -0300

    Update ABB.cpp

--- ABB.cpp
+++ ABB.cpp
@@ -10,96 +10,92 @@ template <typename T> ABB<T>::ABB(Empresa *nueva_empresa,unsigned int pos_rankin
     nodo->pos_ranking = pos_ranking;
 }
 
-template <> void ABB<int>::agregarElemento(Arreglo<Empresa> *empresas,unsigned int pos_ranking){
-    if (nodo == NULL){
-        nodo = new Nodo;
-        nodo->pos_ranking = pos_ranking;
-        empresas->referenciarElemento(nodo->empresa,pos_ranking);
+template <typename T> void ABB<T>::agregarElemento(Empresa *empresas,unsigned int pos_ranking){
+    agregar(nodo,empresas,pos_ranking);
+}
+
+template <> void ABB<int>::agregar(Nodo *&arbol,Empresa *empresas,int pos_ranking){
+    if (arbol == NULL){
+        arbol = new Nodo;
+        arbol->pos_ranking = pos_ranking;
+        arbol->empresa = &empresas[pos_ranking-1];
     }else{
-        if (pos_ranking < nodo->pos_ranking){
-            if (nodo->izq == NULL){
-                nodo->izq = new ABB;
-            }
-            nodo->izq->agregarElemento(empresas,pos_ranking);
+        if (pos_ranking < arbol->pos_ranking){
+            agregar(arbol->izq,empresas,pos_ranking);
         }else{
-            if (nodo->der == NULL){
-                nodo->der = new ABB;
-            }
-            nodo->der->agregarElemento(empresas,pos_ranking);
+            agregar(arbol->der,empresas,pos_ranking);
         }
     }
 }
 
-template <> void ABB<string>::agregarElemento(Arreglo<Empresa> *empresas,unsigned int pos_ranking){
-    if (nodo == NULL){
-        nodo = new Nodo;
-        nodo->pos_ranking = pos_ranking;
-        empresas->referenciarElemento(nodo->empresa,pos_ranking);
+template <> void ABB<string>::agregar(Nodo *&arbol,Empresa *empresas,int pos_ranking){
+    if (arbol == NULL){
+        arbol = new Nodo;
+        arbol->pos_ranking = pos_ranking;
+        arbol->empresa = &empresas[pos_ranking-1];
     }else{
-        Empresa empresa_actual = empresas->obtenerDatos(pos_ranking);
-        if (empresa_actual.obtenerRazonSocial() < nodo->empresa->obtenerRazonSocial()){
-            if (nodo->izq == NULL){
-                nodo->izq = new ABB;
-            }
-            nodo->izq->agregarElemento(empresas,pos_ranking);
+        Empresa empresa_actual = empresas[pos_ranking-1];
+        if (empresa_actual.obtenerRazonSocial() < arbol->empresa->obtenerRazonSocial()){
+            agregar(arbol->izq,empresas,pos_ranking);
         }else{
-            if (nodo->der == NULL){
-                nodo->der = new ABB;
-            }
-            nodo->der->agregarElemento(empresas,pos_ranking);
+            agregar(arbol->der,empresas,pos_ranking);
         }
     }
+}
+
+template <typename T> void ABB<T>::eliminar(Nodo *&arbol){
+   if (arbol != NULL){
+        eliminar(arbol->izq);
+        eliminar(arbol->der);
+        delete arbol;
+        arbol = NULL;
+   }
 }
 
 template <typename T> ABB<T>::~ABB(){
-    if (nodo->der != NULL){
-        delete nodo->der;
-        nodo->der = NULL;
-    }
-    if (nodo->izq != NULL){
-        delete nodo->izq;
-        nodo->izq = NULL;
-    }
-    delete nodo;
-    nodo = NULL;
+    eliminar(nodo);
 }
 
-template <> int ABB<string>::buscarEmpresa(string razon_social) const{
-    if (nodo == NULL){
-        return -1;
-    }else{
-        if (razon_social == nodo->empresa->obtenerRazonSocial()){
-            return nodo->pos_ranking;
-        }else if (razon_social < nodo->empresa->obtenerRazonSocial()){
-            if (nodo->izq != NULL){
-                return nodo->izq->buscarEmpresa(razon_social);
+template <> Empresa* ABB<int>::buscar(Nodo *arbol,int posicion) const{
+    if (arbol != NULL){
+        if (posicion == arbol->pos_ranking){
+            return arbol->empresa;
+        }else if (posicion < arbol->pos_ranking){
+            if (arbol->izq != NULL){
+                return buscar(arbol->izq,posicion);
             }
         }else{
-            if (nodo->der != NULL){
-                return nodo->der->buscarEmpresa(razon_social);
+            if (arbol->der != NULL){
+                return buscar(arbol->der,posicion);
             }
         }
-        return -1;
     }
+    return NULL;
 }
 
-template <> Empresa* ABB<int>::buscarEmpresa(unsigned int posicion) const{
-    if (nodo == NULL){
-        return NULL;
-    }else{
-        if (posicion == nodo->pos_ranking){
-            return nodo->empresa;
-        }else if (posicion < nodo->pos_ranking){
-            if (nodo->izq != NULL){
-                return nodo->izq->buscarEmpresa(posicion);
+template <> int ABB<string>::buscar(Nodo *arbol,string razon_social) const{
+    if (arbol != NULL){
+        if (razon_social == arbol->empresa->obtenerRazonSocial()){
+            return arbol->pos_ranking;
+        }else if (razon_social < arbol->empresa->obtenerRazonSocial()){
+            if (arbol->izq != NULL){
+                return buscar(arbol->izq,razon_social);
             }
         }else{
-            if (nodo->der != NULL){
-                return nodo->der->buscarEmpresa(posicion);
+            if (arbol->der != NULL){
+                return buscar(arbol->der,razon_social);
             }
         }
-        return NULL;
     }
+    return -1;
+}
+
+template <> int ABB<string>::buscarEmpresa(string razon_social){
+    return buscar(nodo,razon_social);
+}
+
+template <> Empresa* ABB<int>::buscarEmpresa(unsigned int posicion){
+    return buscar(nodo,posicion);
 }
 
 template class ABB<int>;
